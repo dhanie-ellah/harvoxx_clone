7d6d84cc205a472a08bb6663b4fab098bd62125d
dhanie-ellah <101949297+dhanie-ellah@users.noreply.github.com>
2022-10-09 11:34:24 -0700

    scroll to top update

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function responsive(){
         hamburgerMenu.className = "links";
     }
 }
+
+let toTop = document.getElementById('totop')
+function scrollToTop(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        toTop.style.display = 'block'
+    } else {
+        toTop.style.display = 'none'
+    }
+}
+
+toTop.addEventListener('click', () => {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+})
+
+window.onscroll = function(){scrollToTop()}
